0e4f355cc980f3cc567400e692b7db58143d8cb9
Nara Oslo Clerc <xelvot@hotmail.fr>
2023-03-16 12:06:40 +0100

    :sparkles: Add internal board representation

--- main.c
+++ main.c
@@ -2,15 +2,30 @@
 #include <stdbool.h>
 
 #define NTURNS 12
+#define NPEGS 4
 
 int current_turn = 0;
+char board[NTURNS][NPEGS];
+char hints[NTURNS][NPEGS];
 
+void init_game(void);
 void draw_board(void);
 void draw_row(bool current);
 
 int main(void) {
+    init_game();
+
     draw_board();
     return 0;
+}
+
+void init_game(void){
+    for(int i = 0; i < NTURNS; i++){
+        for(int j = 0; j < NPEGS; j++){
+            board[i][j] = '.';
+            hints[i][j] = '.';
+        }
+    }
 }
 
 void draw_board(void){
