5bd8d2509c65b18eb94dfd7d446eeac19e41ec23
Nara Oslo Clerc <xelvot@hotmail.fr>
2023-03-16 13:56:28 +0100

    :sparkles: Implement user input of guess into board

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <conio.h>
+#include <stdlib.h>
 
 #define NTURNS 12
 #define NPEGS 4
@@ -11,11 +12,19 @@ char hints[NTURNS][NPEGS];
 void init_game(void);
 void draw_board(void);
 void draw_row(int turn);
+void get_guess(void);
+void flush_input(void);
 
 int main(void) {
     init_game();
 
     draw_board();
+
+    for(current_turn = 0; current_turn < NTURNS; current_turn++){
+        get_guess();
+        draw_board();
+    }
+
     return 0;
 }
 
@@ -47,4 +56,31 @@ void draw_row(int turn){
     printf("| %c %c %c %c | %c%c%c%c |\r\n",
             board[turn][0], board[turn][1], board[turn][2], board[turn][3],
             hints[turn][0], hints[turn][1], hints[turn][2], hints[turn][3]);
+}
+
+void get_guess(void){
+    int valid_input = 0;
+    while(!valid_input){
+        printf("Input your guess: ");
+        
+        valid_input = 1;
+        // A valid input is 4 A-F characters
+        if(scanf("%c%c%c%c", &board[current_turn][0], &board[current_turn][1], &board[current_turn][2], &board[current_turn][3]) == NPEGS){
+            for(int i = 0; i < NPEGS; i++){
+                if(board[current_turn][i] < 'A' || board[current_turn][i] > 'F'){
+                    valid_input = 0;
+                    break;
+                }
+            }
+        }
+        else{
+            valid_input = 0;
+        }
+        flush_input();
+    }
+}
+
+void flush_input(void){
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
