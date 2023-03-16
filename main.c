8a5619e5e19063edcf6bf7b932bcedcf7265ee24
Nara Oslo Clerc <xelvot@hotmail.fr>
2023-03-16 11:46:45 +0100

    :sparkles: Add inert draw_board function

--- main.c
+++ main.c
@@ -1,3 +1,22 @@
+#include <stdio.h>
+
+#define NTURNS 12
+
+void draw_board(void);
+
 int main(void) {
+    draw_board();
     return 0;
+}
+
+void draw_board(void){
+    printf("  ________________\r\n");
+
+    for(int i = 0; i < NTURNS; i++){
+        printf(" | . . . . | .... |\r\n");
+    }
+
+    printf(" |---------|------|\r\n");
+    printf(" |*********|      |\r\n");
+    printf(" |_________|______|\r\n");
 }
