c2be33c6208e1818ba868121a9cfb3a0d264776f
Nara Oslo Clerc <xelvot@hotmail.fr>
2023-03-16 11:56:24 +0100

    :sparkles: Add individual row drawing function

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define NTURNS 12
 
+int current_turn = 0;
+
 void draw_board(void);
+void draw_row(bool current);
 
 int main(void) {
     draw_board();
@@ -13,10 +17,17 @@ void draw_board(void){
     printf("  ________________\r\n");
 
     for(int i = 0; i < NTURNS; i++){
-        printf(" | . . . . | .... |\r\n");
+        draw_row(i == current_turn);
     }
 
     printf(" |---------|------|\r\n");
     printf(" |*********|      |\r\n");
     printf(" |_________|______|\r\n");
+}
+
+void draw_row(bool current){
+    if(current) printf(">");
+    else printf(" ");
+
+    printf("| . . . . | .... |\r\n");
 }
